512ea93bd879440b9be0a0e5f387ebd3d891d938
Gjorge Karakabakov <gorgekara@gmail.com>
2016-09-12 13:33:31 +0200

    Added webpack dev server :sparkles:

--- app/Index.jsx
+++ app/Index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory, IndexRoute, NotFoundRoute } from 'react-router';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 
 import Layout from './components/Layout/Layout.jsx';
 import About from './components/About/About.jsx';
